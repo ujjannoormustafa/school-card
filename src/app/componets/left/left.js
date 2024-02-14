ce56818d968b835083ecb20acdc0d021845eaec0
ujjannoormustafa <ujjannoormustafa123@gmail.com>
2024-02-14 19:53:39 +0500

    update

--- src/app/componets/left/left.js
+++ src/app/componets/left/left.js
@@ -32,39 +32,39 @@ const Left = () => {
     }
     return color;
   };
-
+ 
   // Function to handle image load error
   const handleImageError = (event) => {
     event.target.style.backgroundColor = '#000'; // Set background color to black
   };
 
   return (
-    <div>
-      <h1>Students</h1>
-      <div className="flex justify-center items-center flex-wrap w-8/12">
-        {students.map((student, index) => (
-          <div
-            key={index}
-            className="bg-[#43739e] px-8 flex justify-center py-10 shadow-[0_2px_15px_-6px_rgba(0,0,0,0.2)] w-full max-w-sm rounded-2xl font-[sans-serif] overflow-hidden mx-auto mt-4"
-            style={{ backgroundColor: getRandomColor() }}
-          >
-            <div className="flex flex-col items-center">
-              <img
-                src={student.imageUrl}
-                className="w-60 h-60 rounded-full"
-                alt={student.name}
-                onError={handleImageError}
-              />
-              <div className="mt-6 text-center">
-                <p className="text-base text-gray-100 font-semibold ">{student.name}</p>
-                <h3 className="text-white text-sm mt-4">
-                    {student.detail}                </h3>
+        <div className='px-12 py-10 bg-red-50 '>
+       
+        <div className="flex justify-center items-center flex-wrap w-full">
+          {students.map((student, index) => (
+            <div
+              key={index}
+              className="bg-[#43739e] px-8 flex justify-center py-10 shadow-[0_2px_15px_-6px_rgba(0,0,0,0.2)] w-full max-w-sm rounded-2xl font-[sans-serif] overflow-hidden mx-auto mt-4"
+              style={{ backgroundColor: getRandomColor() }}
+            >
+              <div className="flex flex-col items-center">
+                <img
+                  src={student.imageUrl}
+                  className="w-60 h-60 rounded-full"
+                  alt={student.name}
+                  onError={handleImageError}
+                />
+                <div className="mt-6 text-center">
+                  <p className="text-base text-gray-100 font-semibold">{student.name}</p>
+                  <h3 className="text-white text-sm mt-4">{student.detail}</h3>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+  
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,41 @@
 
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import Left from "./componets/left/left";
-import Nav from "./componets/navbar/nav";
+import { Button, Modal } from 'antd';
+import Nav from './componets/navbar/nav';
 
 export default function Home() {
-  
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex justify-evenly items-center">
-      {/* Right div */}
+    <div className='bg-red-50'>
+        <h1 className='text-center text-5xl mb-4 py-6 font-bold font-mono text-gray-800'>We are Saint Theresa's Students</h1>
 
-     <Left/>
+        <div onClick={() => setOpen(true) }  class="block py-4 text-xl text-white w-60 text-center mx-auto bg-gray-800 px-7 hover:bg-gray-700 rounded-xl">
+        Add Your Info
+</div>
 
-      {/* Left div */}
-      <div className="w-3/12 py-12 flex items-center fixed right-3 top-2 justify-center">
-        <div>
-          <Nav />
-        </div>
-      </div>
+   
+    
+      <Modal
+        centered
+        open={open}
+        okButtonProps={{ style: { display: 'none' } }}
+        cancelText="Close"
+        onCancel={() => setOpen(false)}
+        width={500}
+      >
+       <div>
+        <Nav/>
+       </div>
+      </Modal>
+    <Left/>
+   <p className='text-center text-sm text-gray-400 font-mono'>
+      created by Noor Mustafa Ujjan
+   </p>
     </div>
+   
+  
   );
 }
